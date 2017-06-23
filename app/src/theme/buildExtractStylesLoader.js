e3dcd6a122d4a7db1277faeeb3a9f3575f04e53c
tjmasterson <taylor.j.masterson@gmail.com>
2017-06-23 13:41:45 -0500

    esling update

--- app/src/theme/buildExtractStylesLoader.js
+++ app/src/theme/buildExtractStylesLoader.js
@@ -1,36 +1,36 @@
 function encodeLoader(loader) {
-	  if (typeof loader === 'string') {
-		  return loader;
-	}
+	        if (typeof loader === 'string') {
+		        return loader;
+		}
 
-	  if (typeof loader.options !== 'undefined') {
-		  const query = Object
+	        if (typeof loader.options !== 'undefined') {
+		        const query = Object
 			.keys(loader.options)
 			.map(function map(param) {
-				  return `${encodeURIComponent(param)}=${encodeURIComponent(loader.options[param])}`;
+				        return `${encodeURIComponent(param)}=${encodeURIComponent(loader.options[param])}`;
 			})
 			.join('&');
-		  return `${loader.loader}?${query}`;
+		        return `${loader.loader}?${query}`;
 	}
-	  return loader.loader;
+	        return loader.loader;
 }
 
 module.exports = function buildExtractStylesLoader(loaders) {
-	  const extractTextLoader = encodeLoader(loaders[0]);
-	  const fallbackLoader = encodeLoader(loaders[1]);
+  const extractTextLoader = encodeLoader(loaders[0]);
+  const fallbackLoader = encodeLoader(loaders[1]);
 
-	  const restLoaders = loaders
+  const restLoaders = loaders
 		.slice(2)
 		.map(function map(loader) {
 			  if (typeof loader === 'string') {
 				  return loader;
 			}
-			  return encodeLoader(loader);
+		  return encodeLoader(loader);
 		});
 
-	  return [
-		  extractTextLoader,
-		  fallbackLoader,
-		  ...restLoaders,
-	].join('!');
+  return [
+    extractTextLoader,
+    fallbackLoader,
+		    ...restLoaders,
+	   ].join('!');
 };
